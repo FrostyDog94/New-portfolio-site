3642ee70edd9991049dc31eab2a8523965c33504
Andrew Soguero <andrewsoguero@gmail.com>
2021-09-13 20:25:09 -0600

    Added animations on load

--- app.js
+++ app.js
@@ -9,3 +9,38 @@ function toggleButton() {
 }
 
 hamButton.addEventListener('click', toggleButton);
+
+window.addEventListener('scroll', reveal);
+
+window.addEventListener('load', start);
+
+window.addEventListener('load', startReverse);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active')
+        } else {
+            reveals[i].classList.remove('active')
+        }
+    }
+}
+
+function start() {
+    var starts = document.querySelectorAll('.start');
+    for (var i = 0; i < starts.length; i++) {
+        starts[i].classList.add('active')
+    }
+}
+function startReverse() {
+    var starts = document.querySelectorAll('.startReverse');
+    for (var i = 0; i < starts.length; i++) {
+        starts[i].classList.add('active')
+    }
+}
